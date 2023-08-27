9da311773826ece08d68d085075901ffa1a04e21
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-27 20:21:36 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -7,30 +7,19 @@ import haxe.Log;
  *
  * @author Mihai Alexandru (MAJigsaw77)
  */
-class Main
-{
+class Main {
 	private static final M:Int = 10;
 	private static final N:Int = 10;
 
 	private static final grid:Array<Array<Int>> = [
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	];
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
-	public static function main():Void
-	{
+	public static function main():Void {
 		Log.trace('Original Generation\n');
 
-		for (i in 0...M - 1)
-		{
+		for (i in 0...M - 1) {
 			var buffer:StringBuf = new StringBuf();
 
 			for (j in 0...N - 1)
@@ -41,30 +30,29 @@ class Main
 
 		Log.trace('');
 
-		var future:Array<Array<Int>> = [];
+		var future:Array<Array<Int>> = [
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
 		// Loop through every cell
-		for (l in 0...M - 1)
-		{
-			for (m in 0...N - 1)
-			{
+		for (l in 0...M - 1) {
+			for (m in 0...N - 1) {
 				// Finding no of Neighbours that are alive
 
 				var aliveNeighbours:Int = 0;
 
-				for (i in -1...1)
-				{
-					for (j in -1...1)
-					{
+				for (i in -1...1) {
+					for (j in -1...1) {
 						if ((l + i >= 0 && l + i < M) && (m + j >= 0 && m + j < N))
 							aliveNeighbours += grid[l + i][m + j];
 					}
 				}
-				
+
 				// The cell needs to be subtracted from
 				// its neighbours as it was counted before
 				aliveNeighbours -= grid[l][m];
-				
+
 				// Implementing the Rules of Life
 				if ((grid[l][m] == 1) && (aliveNeighbours < 2))
 					future[l][m] = 0; // Cell is lonely and dies
@@ -79,8 +67,7 @@ class Main
 
 		Log.trace('Next Generation\n');
 
-		for (i in 0...M - 1)
-		{
+		for (i in 0...M - 1) {
 			var buffer:StringBuf = new StringBuf();
 
 			for (j in 0...N - 1)
