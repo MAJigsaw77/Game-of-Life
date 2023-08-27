9f025e95375dd8077d2244d89c4c00a8083294c7
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-08-27 18:53:19 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,9 +1,41 @@
 package;
 
+import haxe.Log;
+
+/**
+ * A simple Haxe program to implement Game of Life
+ *
+ * @author Mihai Alexandru (MAJigsaw77)
+ */
 class Main
 {
+	private static final grid:Array<Array<Int>> = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	];
+
 	public static function main():Void
 	{
-		
+		Log.trace('Original Generation');
+
+		for (i in 0...grid.length)
+		{
+			var buffer:String = '';
+
+			for (j in 0...grid[i].length)
+				buffer += grid[i][j] == 0 ? '.' : '*';
+
+			Log.trace(buffer);
+		}
+
+		Log.trace('');
 	}
 }
